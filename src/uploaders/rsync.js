5076cf480ed19f05a7b5b7dce6f93c907444e711
Clark Tomlinson <fallen013@gmail.com>
2019-07-01 14:40:06 -0400

    try rsync

---
+++ src/uploaders/rsync.js
@@ -0,0 +1,80 @@
+#!/bin/node
+
+/* eslint no-await-in-loop: 0 */
+/* eslint no-constant-condition: 0 */
+const {
+  benchmark, execSync, sleep, isOnline, getFiles,
+} = require('../common.js');
+const {
+  BACKUP_DIR, LOCK_FILE_NAME, WAIT_INTERVAL, DELETE_ON_UPLOAD,
+} = require('../../etc/config.js');
+
+
+const attemptUpload = (filename, opts = {
+  deleteWhenComplete: true,
+  noop: false,
+}) => {
+  try {
+    execSync(`rsync -tvPhr -e 'ssh -p 2225' ${filename} NAS:/volume1/NetBackup/ --progress --ignore-existing `);
+
+    if (opts.deleteWhenComplete) {
+      execSync(`rm ${filename}`);
+    }
+
+    console.log(`Uploaded ${filename}`);
+    return filename;
+  } catch (err) {
+    console.log(`Failed to upload ${filename}`);
+    return false;
+  }
+};
+
+
+const getVideos = files => files.filter(f => f.endsWith('mp4'));
+
+const hasLockFile = files => !!files.find(f => f.includes(LOCK_FILE_NAME));
+
+const uploadVideoFiles = (videos) => {
+  if (!videos.length) {
+    return [];
+  }
+
+  console.log(`Preparing to upload ${videos.length} videos`);
+  const uploadedFiles = videos
+    .map(f => attemptUpload(f, {
+      deleteWhenComplete: DELETE_ON_UPLOAD,
+      noop: false,
+    }))
+    .filter(v => v);
+
+  console.log(`Uploaded ${uploadedFiles.length}/${videos.length}`);
+  return uploadedFiles;
+};
+
+const onlyNewVideos = (uploadHistory, videos, fn) => {
+  // Remove videos that do not exist from the history
+  const newUploadHistory = uploadHistory.filter(f => videos.find(v => f === v));
+  const videosToUpload = videos.filter(v => !newUploadHistory.find(f => f === v));
+
+  const uploadedVideos = fn(videosToUpload);
+
+  return [...newUploadHistory, ...uploadedVideos];
+};
+
+const init = async () => {
+  console.log('TeslaCam rsync Upload daemon');
+
+  let uploadHistory = [];
+
+  while (true) {
+    const files = await getFiles(BACKUP_DIR);
+
+    if (isOnline && !hasLockFile(files)) {
+      benchmark(() => uploadHistory = onlyNewVideos(uploadHistory, getVideos(files), uploadVideoFiles));
+    }
+
+    await sleep(WAIT_INTERVAL);
+  }
+};
+
+init();
